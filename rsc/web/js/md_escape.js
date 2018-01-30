6e293beede49eadd754a6263d2911384613855cc
quantuminati <35411400+quantuminati@users.noreply.github.com>
2018-01-30 09:22:14 -0800

    md_escape.js
    A utility to escape MD text characters.

---
+++ rsc/web/js/md_escape.js
@@ -0,0 +1,21 @@
+(function(){
+	var box = document.createElement("div");
+	var raw = document.createElement("textarea");
+	var escaped = document.createElement("textarea");
+	var btn = document.createElement("button");
+  var elements = [raw, escaped, btn];
+  for (var i=0;i<elements.length;i++) box.append(elements[i]);
+  for (var i=0;i<2;i++) {
+  	elements[i].setAttribute("rows","10");
+	  elements[i].setAttribute("cols","80");
+  }
+	btn.innerText = "Close Me";
+	function closeMe () { box.parentNode.removeChild(box); }
+	btn.onclick = closeMe;
+	function convertText () {
+		escaped.value = raw.value.replace(/([\\`\*_{}\[\]\(\)#\+\-\.\!])/g, "\\$1");
+	}
+	raw.onpaste = function () { setTimeout(convertText, 250)};
+	if (document.body.firstChild) document.body.insertBefore(box, document.body.firstChild);
+	else document.body.appendChild(bopx);
+})();
